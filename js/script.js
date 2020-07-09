489b83d0babd07fc2f49abcbe733b7e50f48baf3
Irina Lazareva <zhulochka@gmail.com>
2020-07-10 00:27:08 +0300

    скрипт

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var openFormButton = document.querySelector(".open-form-button");
+var searchForm = document.querySelector(".search-form");
+var dateinInput = searchForm.querySelector(".datein-input");
+var dateoutInput = searchForm.querySelector(".dateout-input");
+var numberAdultInput = searchForm.querySelector(".number-adult-input");
+var numberChildrenInput = searchForm.querySelector(".number-children-input");
+var todayDate = new Date();
+var message = "";
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("number");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+searchForm.classList.remove("search-form-show");
+searchForm.classList.add("search-form-close");
+
+openFormButton.addEventListener("click", function () {
+  searchForm.classList.toggle("search-form-close");
+  searchForm.classList.toggle("search-form-show");
+  searchForm.classList.remove("search-form-error");
+  dateinInput.focus();
+  if (storage) {
+    numberAdultInput.value = storage;
+  }
+});
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!dateinInput.value || !dateoutInput.value) {
+    evt.preventDefault();
+    var errorMessage = document.createElement('p');
+    searchForm.append(errorMessage);
+    errorMessage.classList.add("error-message");
+    errorMessage.textContent = 'Заполните, пожалуйста, дату';
+    searchForm.classList.remove("search-form-error");
+    searchForm.removeChild("errorMessage");
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add("search-form-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("number", numberAdultInput.value);
+    }
+  }
+});
